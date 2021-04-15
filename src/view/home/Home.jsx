5a5b47244a6d9a5d300a6230ef6a5d97c75e3371
antoinedupuy50 <antoinedupuy50@gmail.com>
2021-04-15 09:50:16 +0200

    Search Bar v1

---
+++ src/view/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from './components/SearchBar';
+
+function Home(props) {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+}
+
+export default Home;
+
+
---
+++ src/view/home/components/SearchBar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Button, makeStyles, TextField } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+
+    root: {
+        '& > *': {
+          margin: theme.spacing(1),
+          width: '50ch',
+          backgroundColor: "white",
+          border: '0'
+        },
+      },
+
+  container: {
+    display: "flex",
+    margin: 'auto',
+    padding: 20,
+    borderRadius: '50px',
+    backgroundColor: "#FADDB6",
+    width: '70%',
+},
+
+  button:{
+      marginLeft: 50,
+      backgroundColor: "white",
+
+
+  }
+}));
+
+function SearchBar(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField
+          id="outlined-basic"
+          label="Votre ville..."
+          variant="outlined"
+        />
+      </form>
+      <Button className={classes.button} variant="contained">
+        Recherchez
+      </Button>
+    </div>
+  );
+}
+
+export default SearchBar;
