7c9c25144020403654a4d6286d13e58f7f86d3d5
antoinedupuy50 <antoinedupuy50@gmail.com>
2021-04-20 15:13:04 +0200

    depot-v1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
+/*
 import Header from "./layout/Header";
-import Footer from "./layout/Footer";
 import Main from "./layout/Main";
+import Footer from "./layout/Footer";
 import Home from "./view/home/Home";
 import MarketPlace from "./view/marketplace/MarketPlace";
 import Connexion from "./common/components/Connexion";
+*/
+import DeposerBien from "./view/deposerbien/DeposerBien";
 
 function App() {
   return (
     <div className="App">
+      {/*
       <Main>
         <Header />
         <Home />
-        {/*<Connexion />
-         <MarketPlace />*/}
+        <Connexion />
+        <MarketPlace />
         <Footer />
       </Main>
+      */}
+      <DeposerBien />
     </div>
   );
 }
