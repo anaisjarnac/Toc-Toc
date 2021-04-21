51301f7ae5b8abd5cc21f43d311fff74ce8c1bf5
NicolasGeneste <nicolasgeneste@gmail.com>
2021-04-21 14:24:21 +0200

    filter

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
     <div className="App">
       <Main>
         <Header />
-        <Home />
-        {/*<Connexion />
-         <MarketPlace />*/}
+        {/*<Home />
+        <Connexion />*/}
+        <MarketPlace />
         <Footer />
       </Main>
     </div>
