f896c1a794232b2ec19249fca244cf644d98e74d
antoinedupuy50 <antoinedupuy50@gmail.com>
2021-04-22 14:27:59 +0200

    addnewflat

--- src/App.js
+++ src/App.js
@@ -1,40 +1,57 @@
-import React from "react";
-import Inscription from "./view/inscription/Inscription";
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Main from "./layout/Main";
-import Footer from "./layout/Footer";
-import Home from "./view/home/Home";
-import MarketPlace from "./view/marketplace/MarketPlace";
-import Connexion from "./common/components/Connexion";
-import DeposerBien from "./view/deposerbien/DeposerBien";
+import React from 'react';
+// import Inscription from './view/inscription/Inscription';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Main from './layout/Main';
+// import Home from './view/home/Home';
+// import MarketPlace from './view/marketplace/MarketPlace';
+// import Connexion from './common/components/Connexion';
+import DeposerBien from './view/deposerbien/DeposerBien';
 
 function App() {
   return (
-
-    <Router>
-      <Switch>
-        <Route exact path="/" render={() => (
-          <Main>
-              <Home />
-          </Main>
-        )} />
-          <Route exact path="/marketplace" render={() => (
-          <Main>
-              <MarketPlace />
-          </Main>
-        )} />
-        <Route exact path="/connexion" render={() => (
-          <Main>
-              <Connexion />
-          </Main>
-        )} />
-        <Route exact path="/inscription" render={() => (
-          <Main>
-              <Inscription />
-          </Main>
-        )} />
-      </Switch>
-    </Router>
+    // <Router>
+    //   <Switch>
+    //     <Route
+    //       exact
+    //       path="/"
+    //       render={() => (
+    //         <Main>
+    //           <Home />
+    //         </Main>
+    //       )}
+    //     />
+    //     <Route
+    //       exact
+    //       path="/marketplace"
+    //       render={() => (
+    //         <Main>
+    //           <MarketPlace />
+    //         </Main>
+    //       )}
+    //     />
+    //     <Route
+    //       exact
+    //       path="/connexion"
+    //       render={() => (
+    //         <Main>
+    //           <Connexion />
+    //         </Main>
+    //       )}
+    //     />
+    //     <Route
+    //       exact
+    //       path="/inscription"
+    //       render={() => (
+    //         <Main>
+    //           <Inscription />
+    //         </Main>
+    //       )}
+    //     />
+    //   </Switch>
+    // </Router>
+    <div>
+      <DeposerBien/>
+    </div>
   );
 }
 
