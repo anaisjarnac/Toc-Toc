b3dd584e0d079226f1b473c646a4a7d95157cbff
Anaïs Jarnac <ajarnac@gmail.com>
2021-04-23 08:26:23 +0200

    menuburgerlien

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import {Link} from "react-router-dom";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 import BurgerMenu from "../view/home/components/BurgerMenu";
 
 const useStyles = makeStyles((theme) => ({
@@ -33,7 +33,7 @@ const useStyles = makeStyles((theme) => ({
 
   title: {
     color: "#4E96AF",
-    fontFamily: "'Fredoka One', cursive",
+    fontFamily: "'Comfortaa', cursive",
     fontSize: "40px",
   },
 
@@ -47,18 +47,16 @@ function Header() {
   const classes = useStyles();
   return (
     <nav className={classes.sticky}>
-         <Link to="/"> 
-            <img
-                src="/assets/img/logoheader.png"
-                alt="logo"
-                className={classes.toctoclogo}
-            />
-        </Link>
+      <Link to="/">
+        <img
+          src="/assets/img/logoheader.png"
+          alt="logo"
+          className={classes.toctoclogo}
+        />
+      </Link>
       <h1 className={classes.title}>Toc Toc</h1>
       <Button className={classes.button} variant="contained" color="primary">
-        <Link to="/connexion">
-            Connexion
-        </Link>
+        <Link to="/connexion">Connexion</Link>
       </Button>
       <BurgerMenu />
     </nav>
--- src/view/inscription/components/SelectOption.jsx
+++ src/view/inscription/components/SelectOption.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -15,28 +15,40 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SelectOption() {
   const classes = useStyles();
+  const [userSelect, setUserSelect] = React.useState("");
+  const [userSelectToo, setUserSelectToo] = React.useState("");
+  console.log(userSelect);
 
   return (
     <div>
       <FormControl className={classes.formControl}>
         <InputLabel htmlFor="grouped-native-select">Tu es</InputLabel>
-        <Select native defaultValue="" id="grouped-native-select">
+        <Select
+          native
+          defaultValue=""
+          id="grouped-native-select"
+          onChange={(e) => setUserSelect(e.target.value)}
+        >
           <option aria-label="None" value="" />
           <optgroup>
-            <option value={1}>Locataire</option>
-            <option value={2}>Propriétaire</option>
+            <option value={"locataire"}>Locataire</option>
+            <option value={"proprietaire"}>Propriétaire</option>
           </optgroup>
         </Select>
       </FormControl>
       <FormControl className={classes.formControl}>
         <InputLabel htmlFor="grouped-select">Mais aussi</InputLabel>
-        <Select defaultValue="" id="grouped-select">
+        <Select
+          defaultValue=""
+          id="grouped-select"
+          onChange={(e) => setUserSelectToo(e.target.value)}
+        >
           <MenuItem value="">
             <em>Rien</em>
           </MenuItem>
           <ListSubheader></ListSubheader>
-          <MenuItem value={1}>Locataire</MenuItem>
-          <MenuItem value={2}>Propriétaire</MenuItem>
+          <MenuItem value={"locataire"}>Locataire</MenuItem>
+          <MenuItem value={"proprietaire"}>Propriétaire</MenuItem>
         </Select>
       </FormControl>
     </div>
